f953cdbb1b3a1c2d6b470133958cdb1797b522db
idrissaSougrinoma <ouedriss1@gmail.com>
2026-09-19 01:11:30 +0000

    mise à jour 19

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,7 @@ class HomeController extends Controller
             ->limit(6)
             ->get();
 
-        $categoriesDocuments = CategorieDocument::whereNull('parent_id')->get();
+        // $categoriesDocuments = CategorieDocument::whereNull('parent_id')->get();
 
         $documentsRecents = Document::with('categorie')
             ->orderByDesc('publie_le')
@@ -69,7 +69,7 @@ class HomeController extends Controller
                 ->distinct()
                 ->get()
                 ->count(),
-            'thematiques' => CategorieDocument::whereNull('parent_id')->count(),
+            // 'thematiques' => CategorieDocument::whereNull('parent_id')->count(),
         ];
 
         $typeLabels = [
@@ -99,14 +99,14 @@ class HomeController extends Controller
             'partenaires'         => $partenaires,
         ]);
 
-        
+
     }
 
     // Mot du Directeur
     public function motDuDirecteur()
     {
         $param_site = ParametresSite::first();
- 
+
         return view('mot_directeur.index', [
             'param_site' => $param_site,
         ]);
