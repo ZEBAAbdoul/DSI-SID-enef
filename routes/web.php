5657de6f12aeef042160922719d10273c02301f3
Tiboss-Art <digihouse10@gmail.com>
2026-09-23 09:30:45 +0000

    gestion de contact fins

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\BibliothequeController;
 use App\Http\Controllers\CatalogueController;
-<<<<<<< HEAD
 use App\Http\Controllers\ContactController;
-=======
 use App\Http\Controllers\DocumentController;
->>>>>>> 4293f371683a47d5c93ec9250a2d4e01ebb2af9e
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InformationsComplementairesController;
 use App\Http\Controllers\LoginWithOTPController;
@@ -71,7 +68,7 @@ Route::get('/manual', [ManualController::class, 'index'])->name('manual.index');
 
 Route::get('/formations/informations-complementaires', [InformationsComplementairesController::class, 'index'])
     ->name('formations.informations');
-    
+
 Route::get('/formations/{formation:slug}', function (\App\Models\Formation $formation) {
     $formation->load(['filiere', 'categorie']);
     $param_site = ParametresSite::first();
